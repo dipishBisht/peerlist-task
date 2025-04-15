d78c3146ece9e4580010ed1575d05b20fc29f837
dipishBisht <dipish1718@gmail.com>
2025-04-16 01:59:55 +0530

    task 4 added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div>
       <h1>All Day Projects</h1>
       <ol>
-        <li><Link href="/day-1">Day  1</Link></li>
+        {/* <li><Link href="/day-1">Day  1</Link></li> */}
+        <li><Link href="/day-4">Day 4</Link></li>
       </ol>
     </div>
   );
